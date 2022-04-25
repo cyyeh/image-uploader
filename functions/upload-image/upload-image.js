7e1a2a0ee994ff2d7cfb720ead33cfde046eb699
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2022-04-25 19:19:32 +0800

    add uploading image

--- functions/upload-image/upload-image.js
+++ functions/upload-image/upload-image.js
@@ -1,10 +1,12 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const body = JSON.parse(event.body)
+    const name = body.name
+    const base64 = body.base64
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ name, base64 }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
--- index.js
+++ index.js
@@ -1,37 +1,78 @@
 window.onload = function() {
   // DOM element declarations
   const $UploadContainer = document.getElementById('uploader-container')
+  const $DragDropContainer = document.getElementById('drag-drop-container')
   const $FileInput = document.getElementById('file-input')
   const $LoadingContainer = document.getElementById('loading-container')
   const $UploadingResultContainer = document.getElementById('uploading-result-container')
+  const $UploadedImage = document.querySelector('.uploaded-img')
 
-  const API_ENDPOINT = 'https://image-uploader-cyyeh.netlify.app/.netlify/functions/upload-image'
+  const API_ENDPOINT = `${document.location.origin}/.netlify/functions/upload-image`
 
-  // DOM element event listener
-  $FileInput.addEventListener('change', function() {
+  const convertFile = (file) => {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader()
+      reader.onload = () => {
+        resolve(reader.result)
+      }
+      reader.onerror = () => {
+        reject(reader.error)
+      }
+      reader.readAsDataURL(file)
+    })
+  }
+
+  const handlePhotoUpload = async (photo) => {
     $UploadContainer.style.display = 'none'
     $LoadingContainer.style.display = 'block'
 
-    let photo = this.files[0]
-    let formData = new FormData()
-    formData.append('photo', photo)
+    const base64Str = await convertFile(photo)
     fetch(
       API_ENDPOINT,
       {
         method: 'POST',
-        body: formData
+        body: JSON.stringify({
+          "name": photo.name,
+          "base64": base64Str
+        })
       },
     )
       .then(response => response.json())
       .then(function(data) {
         console.log(data)
+        $UploadedImage.style.backgroundImage = `url(${data.base64})`
         $LoadingContainer.style.display = 'none'
         $UploadingResultContainer.style.display = 'block'
       })
       .catch(function(error) {
-        console.lerror(error)
+        console.error(error)
         $LoadingContainer.style.display = 'none'
         $UploadContainer.style.display = 'block'
       })
+  }
+
+  // DOM element event listener
+  $DragDropContainer.addEventListener('dragenter', function(e) {
+    e.stopPropagation()
+    e.preventDefault()
+  }, false)
+
+  $DragDropContainer.addEventListener('dragover', function(e) {
+    e.stopPropagation()
+    e.preventDefault()
+  }, false)
+
+  $DragDropContainer.addEventListener('drop', async function(e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    const data = e.dataTransfer
+    const photo = data.files[0]
+    await handlePhotoUpload(photo)
+  }, false)
+
+  $FileInput.addEventListener('change', async function() {
+    const photo = this.files[0]
+    await handlePhotoUpload(photo)
   }, false)
 }
