504e7c04a3b2bd09604278d1bca7cbd157e1edfb
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2022-04-25 16:22:40 +0800

    finish styling

---
+++ index.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+  // DOM element declarations
+  const $UploadContainer = document.getElementById('uploader-container')
+  const $FileInput = document.getElementById('file-input')
+  const $LoadingContainer = document.getElementById('loading-container')
+  const $UploadingResultContainer = document.getElementById('uploading-result-container')
+
+  const API_ENDPOINT = ''
+
+  // DOM element event listener
+  $FileInput.addEventListener('change', function() {
+    $UploadContainer.style.display = 'none'
+    $LoadingContainer.style.display = 'block'
+
+    let photo = this.files[0]
+    let formData = new FormData()
+    formData.append('photo', photo)
+    fetch(
+      API_ENDPOINT,
+      {
+        method: 'POST',
+        body: formData
+      },
+    )
+      .then(response => response.json())
+      .then(function(data) {
+        console.log(data)
+        $LoadingContainer.style.display = 'none'
+        $UploadingResultContainer.style.display = 'block'
+      })
+      .catch(function(error) {
+        console.lerror(error)
+        $LoadingContainer.style.display = 'none'
+        $UploadContainer.style.display = 'block'
+      })
+  }, false)
+}
